80a4d9e357909824eabedbc00b8e2cceb0eaf617
MihaiElisei <mihai.m.elisei@gmail.com>
2022-09-08 08:40:19 +0000

    Add function for player to chose the symbol that he wants to play with

--- run.py
+++ run.py
@@ -6,3 +6,15 @@ def boardLayout(board): #prints out the board
     print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
     print('-----------')
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+
+#Player choose letter X or O
+def playerSymbol():
+    letter = ''
+    while not (letter == 'X' or letter == 'O'):
+        print('Do you want to play with X or O?')
+        letter = input().upper()
+    if letter == 'X':
+        return ['X', 'O'] #Player have X and computer O
+    else:
+        return ['O', 'X'] #Computer have X and player O
+
